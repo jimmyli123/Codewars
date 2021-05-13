7d357f847503895e03aa46737fbc68f34e01b737
Jimmy Li <lijjimmy@gmail.com>
2021-05-13 19:09:12 -0400

    adding an 8kyu

--- js/8kyu.js
+++ js/8kyu.js
@@ -1,7 +1,14 @@
 
-
 // Convert a Number to a String!
 
 function numberToString(num) {
     return String(num)
+  }
+
+
+// Convert boolean values to strings 'Yes' or 'No'.
+
+function boolToWord( bool ){
+    if (bool) { return 'Yes'}
+    else { return 'No'}
   }
