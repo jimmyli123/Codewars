5cf5345efce59203e96c1ce2e6caddad11bbdb20
Jimmy Li <lijjimmy@gmail.com>
2021-06-02 22:20:59 -0400

    solved an 8kyu

--- js/8kyu.js
+++ js/8kyu.js
@@ -20,3 +20,18 @@ function bonusTime(salary, bonus) {
       if (bonus) { return `£${salary * 10}`}
       else { return `£${salary}`}
     }
+
+    
+// Compare within margin
+function closeCompare(a, b, margin){
+  switch(true) {
+      case Math.abs(a-b) <= margin:
+      return 0;
+      case a < b:
+      return -1;
+      case a > b:
+      return 1;
+      default:
+      return 0;
+  }
+}
