81bcecab3862b0bbfd949d7c71f78b6f6fdf4dd8
Jimmy Li <lijjimmy@gmail.com>
2021-06-05 23:32:01 -0400

    a simple 8kyu

--- js/8kyu.js
+++ js/8kyu.js
@@ -35,3 +35,17 @@ function closeCompare(a, b, margin){
       return 0;
   }
 }
+
+// Quarter of the year
+const quarterOf = (month) => {
+  switch (true) {
+      case (month <= 3):
+      return 1;
+      case (month <= 6):
+      return 2;
+      case (month <= 9):
+      return 3;
+      case (month <= 12):
+      return 4
+  }
+}
