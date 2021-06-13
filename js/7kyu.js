e32a18b6ae392992ff9a5562c5b1c588de455542
Jimmy Li <lijjimmy@gmail.com>
2021-06-12 23:21:22 -0400

    solved another 7 kyu. First try

--- js/7kyu.js
+++ js/7kyu.js
@@ -255,3 +255,17 @@ function solution(str, ending){
     }
     return count;
   }
+
+  // Categorize New Member
+  function openOrSenior(data){
+    let answer = []
+    for (let i =0; i < data.length; i++) {
+      if (data[i][0] >= 55 && data[i][1] > 7) {
+        answer.push("Senior")
+      }
+      else {
+        answer.push("Open")
+      }
+    }
+    return answer
+  }
