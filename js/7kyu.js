abbe227293e9ff270cbe266cf286c9f112bbea8e
Jimmy Li <lijjimmy@gmail.com>
2021-06-15 12:00:27 -0400

    solved 7kyu

--- js/7kyu.js
+++ js/7kyu.js
@@ -289,3 +289,10 @@ function fizzbuzz(n)
   }
   return answerArray
 }
+
+// List filtering
+
+function filter_list(l) {
+  // Return a new array with the strings filtered out
+  return l.filter(el => typeof(el) === 'number')
+}
