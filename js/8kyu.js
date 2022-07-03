a36e0e6bef6a11436c05b76373a09f74393824f5
Jimmy <lijjimmy@gmail.com>
2022-07-03 01:41:35 -0400

    solved another one

--- js/8kyu.js
+++ js/8kyu.js
@@ -160,3 +160,11 @@ function setAlarm(employed, vacation){
   }
   else {return false;}
 }
+
+// Simple multiplication
+function simpleMultiplication(number) {
+  if (number%2 ===0) {
+    return number * 8;
+  }
+  else { return number*9}
+}
